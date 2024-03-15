c137d68d5656ead32ba17af6846f773cafe335ac
elmambou <esmambou@gmail.com>
2024-03-15 20:47:44 +0200

    clearing connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,32 +1,31 @@
+const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
 
 let _db;
 
-const initDb = (callback) => {
-  if (_db) {
-    console.log('Db is already initialized!');
-    return callback(null, _db);
-  }
-  MongoClient.connect(process.env.MONGODB_URI)
-    .then((client) => {
-      _db = client;
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
-    });
+const initDb = async () => {
+    if (!_db) {
+        try {
+            const client = await MongoClient.connect(process.env.MONGODB_URI);
+            _db = client.db();
+            console.log('Connected to MongoDB');
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error);
+            throw error;
+        }
+    }
+    return _db;
 };
 
 const getDb = () => {
-  if (!_db) {
-    throw Error('Db not initialized');
-  }
-  return _db;
+    if (!_db) {
+        throw Error('DB not initialized!');
+    }
+    return _db;
 };
 
 module.exports = {
-  initDb,
-  getDb
+    initDb,
+    getDb
 };
