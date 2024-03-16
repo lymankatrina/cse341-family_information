d5f75cf89e27f0a5c0ee937d80f625677e5709b9
Katrina Lyman <lymankatrina@gmail.com>
2024-03-16 12:05:25 -0600

    fix connect.js file

--- db/connect.js
+++ db/connect.js
@@ -1,26 +1,27 @@
-const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
+const MongoClient = require('mongodb').MongoClient;
 
 let _db;
 
-const initDb = async () => {
-  if (!_db) {
-    try {
-      const client = await MongoClient.connect(process.env.MONGODB_URI);
-      _db = client.db();
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-      throw error;
-    }
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
   }
-  return _db;
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
 };
 
 const getDb = () => {
   if (!_db) {
-    throw Error('DB not initialized!');
+    throw Error('Db not initialized');
   }
   return _db;
 };
