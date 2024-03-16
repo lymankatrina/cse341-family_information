4eeb28f26255c4dfdd2cbfb9aea6bc988e7eec0b
elmambou <esmambou@gmail.com>
2024-03-16 14:47:35 +0200

    changes to swagger.js

--- swagger.js
+++ swagger.js
@@ -1,107 +1,41 @@
-const swaggerAutogen = require('swagger-autogen')();
-
-const doc = {
-  info: {
-    title: 'Our Team Final Project CSE 341',
-    description: 'Family Information API'
-  },
-  host: 'cse341-family_information.onrender.com',
-  schemes: ['https'],
-  consumes: ['application/json'],
-  produces: ['application/json'],
-  tags: [
-    {
-      name: 'Individuals',
-      description: 'Operations related to individuals'
-    },
-  ],
-  definitions: {
-    Individuals: {
-      type: 'object',
-      properties: {
-        firstName: { type: 'string' },
-        middleName: { type: 'string' },
-        lastName: { type: 'string' },
-        birthDate: { type: 'string' },
-        parents: { type: 'string' },
-        email: { type: 'string' },
-        household: { type: 'string' },
-        headOfHousehold: { type: 'string' },
-        picture: { type: 'string' }
-      },
-      required: ['firstName', 'middleName', 'lastName', 'birthDate', 'parents', 'email', 'household', 'headOfHousehold', 'picture']
-    }
-  },
-  paths: {
-    '/individuals/{id}': {
-      put: {
-        tags: ['Individuals'],
-        summary: 'Update an individual',
-        parameters: [
-          {
-            name: 'id',
-            in: 'path',
-            description: 'ID of the individual to update',
-            required: true,
-            type: 'string'
-          },
-          {
-            in: 'body',
-            name: 'body',
-            description: 'Updated individual object',
-            required: true,
-            schema: {
-              $ref: '#/definitions/Individuals'
-            }
-          }
-        ],
-        responses: {
-          204: {
-            description: 'Individual updated successfully'
-          },
-          400: {
-            description: 'Bad Request'
-          },
-          412: {
-            description: 'Precondition Failed'
-          },
-          500: {
-            description: 'Internal Server Error'
-          }
-        }
-      },
-      delete: {
-        tags: ['Individuals'],
-        summary: 'Delete an individual',
-        parameters: [
-          {
-            name: 'id',
-            in: 'path',
-            description: 'ID of the individual to delete',
-            required: true,
-            type: 'string'
-          }
-        ],
-        responses: {
-          200: {
-            description: 'Individual deleted successfully'
-          },
-          400: {
-            description: 'Bad Request'
-          },
-          412: {
-            description: 'Precondition Failed'
-          },
-          500: {
-            description: 'Internal Server Error'
-          }
-        }
+const options = {
+    openapi: '3.0.0',
+    autoHeaders: true,
+    autoQuery: true,
+    autoBody: true
+  };
+  const swaggerAutogen = require('swagger-autogen')(options);
+  
+  const doc = {
+    info: {
+      version: '1.0.0',
+      title: 'Family Information API',
+      description:
+        'This is a family information app created by team d for the CSE341 team project assignment',
+      contact: {
+        name: 'CSE 341 Team D',
+        url: 'https://github.com/ajrobbins04/cse341-family_information'
       }
-    }
-  }
-};
-
-const outputFile = './swagger-output.json';
-const endpointsFiles = ['./routes/individualRoutes.js']; // Change the endpoint file here
-
-swaggerAutogen(outputFile, endpointsFiles, doc);
+    },
+    servers: [
+      {
+        url: 'https://cse341proj2.onrender.com/',
+        description: 'Render website'
+      },
+      {
+        url: 'http://localhost:3000',
+        description: 'Local development server'
+      }
+    ],
+    paths: {},
+    schemes: {},
+    produces: {},
+    tags: [],
+    definitions: {},
+    components: {}
+  };
+  
+  const outputFile = './swagger-output.json';
+  const endpointsFile = ['./routes/index.js'];
+  
+  swaggerAutogen(outputFile, endpointsFile, doc);
---
+++ swagger1.js
@@ -0,0 +1,107 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Our Team Final Project CSE 341',
+    description: 'Family Information API'
+  },
+  host: 'cse341-family_information.onrender.com',
+  schemes: ['https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: 'Individuals',
+      description: 'Operations related to individuals'
+    },
+  ],
+  definitions: {
+    Individuals: {
+      type: 'object',
+      properties: {
+        firstName: { type: 'string' },
+        middleName: { type: 'string' },
+        lastName: { type: 'string' },
+        birthDate: { type: 'string' },
+        parents: { type: 'string' },
+        email: { type: 'string' },
+        household: { type: 'string' },
+        headOfHousehold: { type: 'string' },
+        picture: { type: 'string' }
+      },
+      required: ['firstName', 'middleName', 'lastName', 'birthDate', 'parents', 'email', 'household', 'headOfHousehold', 'picture']
+    }
+  },
+  paths: {
+    '/individuals/{id}': {
+      put: {
+        tags: ['Individuals'],
+        summary: 'Update an individual',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            description: 'ID of the individual to update',
+            required: true,
+            type: 'string'
+          },
+          {
+            in: 'body',
+            name: 'body',
+            description: 'Updated individual object',
+            required: true,
+            schema: {
+              $ref: '#/definitions/Individuals'
+            }
+          }
+        ],
+        responses: {
+          204: {
+            description: 'Individual updated successfully'
+          },
+          400: {
+            description: 'Bad Request'
+          },
+          412: {
+            description: 'Precondition Failed'
+          },
+          500: {
+            description: 'Internal Server Error'
+          }
+        }
+      },
+      delete: {
+        tags: ['Individuals'],
+        summary: 'Delete an individual',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            description: 'ID of the individual to delete',
+            required: true,
+            type: 'string'
+          }
+        ],
+        responses: {
+          200: {
+            description: 'Individual deleted successfully'
+          },
+          400: {
+            description: 'Bad Request'
+          },
+          412: {
+            description: 'Precondition Failed'
+          },
+          500: {
+            description: 'Internal Server Error'
+          }
+        }
+      }
+    }
+  }
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./routes/individualRoutes.js']; // Change the endpoint file here
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
