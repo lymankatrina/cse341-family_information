977321064fa2712e8b214088edcf1a3b14f8c6aa
elmambou <esmambou@gmail.com>
2024-03-16 09:39:52 +0200

    changes to .env

--- server3.js
+++ server3.js
@@ -11,16 +11,18 @@ const port = process.env.PORT || 3000;
 app
   .use(cors())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
   .use('/', require('./routes'))
 
-mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port); 
-    console.log(`Connected to DB and Web Server is running on port ${port}`);
-  }
+
+  mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port); 
+      console.log(`Connected to DB and listening on ${port}`);
+     
+    }
 });
