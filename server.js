82aa0e59acb904be6450ef6ed7c232d6ad44697d
elmambou <esmambou@gmail.com>
2024-03-16 10:21:29 +0200

    changing application port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongodb = require('./db/connect');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 
 app
   .use(cors())
