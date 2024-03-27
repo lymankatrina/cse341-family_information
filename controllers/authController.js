ba0925d547ecf5fccf417a9617e28e16e0a9504d
Katrina Lyman <lymankatrina@gmail.com>
2024-03-26 19:41:29 -0600

    Basic AuthO Setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,20 @@
 // this file becomes more useful when authentication is necessary to gain access...
-const landingPage = (req, res) => {
+
+const checkAuth = (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? `Logged In: Authentication successful. Welcome to the family information directory! Add '/api-docs' to the url to view API documentation!` : 'Logged out');
+};
+
+const callback = (req, res) => {
   res.send(
-    `Welcome to the family information directory! Add '/api-docs' to the url to view API documentation!`
+    `Authentication successful. Welcome to the family information directory! Add '/api-docs' to the url to view API documentation!`
   );
 };
 
-module.exports = { landingPage };
+const getProfile = (req, res) => {
+  res.status(200).json({
+    userProfile: req.oidc.user,
+    title: 'Profile page'
+  })
+};
+
+module.exports = { callback, checkAuth, getProfile };
